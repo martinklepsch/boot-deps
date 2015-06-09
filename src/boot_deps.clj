9c045febc3a6a0979a5933dbf9c5d82ba304a2ad
stask <stask@me.com>
2015-06-09 16:55:19 +0300

    added 'latest' task

--- src/boot_deps.clj
+++ src/boot_deps.clj
@@ -36,3 +36,24 @@
         (util/info "Currently using %s but %s is available\n"
           (pr-str (:form artifact)) (:version-string new))))
     fileset))
+
+(defn find-latest [env library opts]
+  (let [ancient-pod (make-ancient-pod)
+        {:keys [repositories]} env]
+    (pod/with-eval-in ancient-pod
+      (require '[ancient-clj.core :as ancient])
+      (ancient/latest-version-string! (quote ~library)
+                                      (assoc ~opts :repositories ~repositories)))))
+
+(deftask latest
+  "Find latest version of given library"
+  [l library   VAL sym  "library identifier"
+   s snapshots     bool "allow SNAPSHOT versions to be reported as new"
+   q qualified     bool "allow alpha, beta, etc... versions to be reported as new"
+   a all           bool "allow SNAPSHOT and qualified versions to be reported as new"]
+  (boot/with-pre-wrap fileset
+    (util/info (str "Searching for latest versions of [" library "]...:"))
+    (let [opts {:snapshots? (or snapshots all)
+                :qualified? (or qualified all)}]
+      (util/info " %s\n" (find-latest (boot/get-env) library opts)))
+    fileset))
